5b7af08cb1eb9c52ceb84a441a119b05dbc79ff7
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-10-04 20:03:11 +0200

    feat: benchmark dependency

--- project/benchmark/main.cpp
+++ project/benchmark/main.cpp
@@ -1,13 +1,3 @@
-#include <iostream>
-#include <string>
-
-#include "peac/peac.hpp"
-
 int main(){
-    const std::string iniFileName("configs/peac-office.ini");
-    auto algorithm = peac::PEAC(iniFileName);
-
-    std::cout << algorithm.run() << '\n';
-
     return 0;
 }
